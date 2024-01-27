e79bb4bef96109589f3ecf1dc93d7802f404fd86
Dmitriy Mekh <codemekh@gmail.com>
2024-01-28 01:32:41 +0600

    Initialized Mongoose

---
+++ src/DB/index.ts
@@ -0,0 +1,14 @@
+import Mongoose from 'mongoose';
+
+Mongoose.Promise = global.Promise;
+
+export async function init() {
+  try {
+    await Mongoose.connect(process.env.DB_URI || '');
+    console.log('Connected to MongoDB');
+    console.log('Successfully connected to MongoDB!');
+  } catch (error) {
+    console.error('Connection to MongoDB failed!', error);
+    process.exit();
+  }
+}
