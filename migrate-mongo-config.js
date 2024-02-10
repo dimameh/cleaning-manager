886812a274f0dee9bca1e18315a76483cc1aff26
Dmitriy Mekh <codemekh@gmail.com>
2024-02-11 01:50:56 +0600

    updated mongo migrate config

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -21,7 +21,7 @@ const config = {
   changelogCollectionName: "changelog",
 
   // The file extension to create migrations and search for in migration dir 
-  migrationFileExtension: ".ts",
+  migrationFileExtension: ".js",
 
   // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determine
   // if the file should be run.  Requires that scripts are coded to be run multiple times.
