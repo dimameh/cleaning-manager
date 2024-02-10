d1a1b0ae94f936fb59a376134ac2cac47250f52a
Dmitriy Mekh <codemekh@gmail.com>
2024-02-10 16:24:31 +0600

    Added db migrations config

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -4,10 +4,13 @@ Mongoose.Promise = global.Promise;
 
 export async function initDB() {
   try {
-    if (!process.env.DB_URI) {
-      throw new Error('DB_URI is not defined!');
+    if (!process.env.DB_URL) {
+      throw new Error('DB_URL is not defined!');
     }
-    await Mongoose.connect(process.env.DB_URI);
+    if (!process.env.DB_NAME) {
+      throw new Error('DB_NAME is not defined!');
+    }
+    await Mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`);
     console.log('Connected to DB');
   } catch (error) {
     console.error('Connection to DB failed!', error);
